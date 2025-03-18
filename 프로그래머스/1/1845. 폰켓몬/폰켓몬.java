5b1c7958eefd3618f80aa668954f51f480cc71ab
김영관 <young.kwin@gmail.com>
2025-03-18 14:40:57 +0900

    [level 1] Title: 폰켓몬, Time: 4.79 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> num = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            num.add(nums[i]);
+        }
+        if(num.size()>nums.length/2)
+            answer=nums.length/2;
+        else
+            answer=num.size();
+        return answer;
+    }
+}
