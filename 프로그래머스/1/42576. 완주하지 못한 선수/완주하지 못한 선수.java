4c56f8243aef612de240970ad7d9b7e7b7b33c81
김영관 <young.kwin@gmail.com>
2025-03-19 15:55:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.11 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,25 @@
-import java.util.*;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for(int i=0; i<participant.length; i++){
-            if(i==participant.length-1 || !participant[i].equals(completion[i])){
-                answer=participant[i];
-                break;
+        HashMap<String, Integer> people = new HashMap<>();
+
+        // 참가자 카운트
+        for (String p : participant) {
+            people.put(p, people.getOrDefault(p, 0) + 1);
+        }
+
+        // 완주자 제거
+        for (String c : completion) {
+            people.put(c, people.get(c) - 1);
+        }
+
+        // 남은 한 명 찾기
+        for (String key : people.keySet()) {
+            if (people.get(key) > 0) {
+                return key;
             }
         }
-        return answer;
+        return "";
     }
 }
