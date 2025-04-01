46e9b3430342bdc2a3c6942e6a508d380341b84b
김영관 <young.kwin@gmail.com>
2025-04-01 12:34:33 +0900

    [level 1] Title: 실패율, Time: 5.28 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,36 +1,47 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        double[] tempArr = new double[N];
-        int arrLength = stages.length;
-        int idx = arrLength;
-        double tempD = 0;
-        int tempI = 0;
-        for (int i = 0; i < arrLength; i++) {
-            int stage = stages[i];
-            if (stage != N + 1)
-                answer[stage - 1] += 1;
-        }
-        for (int i = 0; i < N; i++) {
-            int personNum = answer[i];
-            tempArr[i] = (double) personNum / idx;
-            idx -= personNum;
-            answer[i] = i + 1;
+        int[] stageCount = new int[N + 2]; // 인덱스: 스테이지 번호 (1~N), N+1은 클리어한 사람
+        for (int stage : stages) {
+            stageCount[stage]++;
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j < N - i; j++) {
-                if (tempArr[j - 1] < tempArr[j]) {
-                    tempD = tempArr[j - 1];
-                    tempArr[j - 1] = tempArr[j];
-                    tempArr[j] = tempD;
+        List<Stage> stageList = new ArrayList<>();
+        int totalPlayers = stages.length;
 
-                    tempI = answer[j - 1];
-                    answer[j - 1] = answer[j];
-                    answer[j] = tempI;
-                }
+        for (int i = 1; i <= N; i++) {
+            int count = stageCount[i];
+            double failureRate = totalPlayers == 0 ? 0 : (double) count / totalPlayers;
+            stageList.add(new Stage(i, failureRate));
+            totalPlayers -= count;
+        }
+
+        // 실패율 기준 내림차순 정렬, 같으면 스테이지 번호 오름차순
+        stageList.sort((a, b) -> {
+            if (b.rate == a.rate) {
+                return Integer.compare(a.num, b.num);
             }
+            return Double.compare(b.rate, a.rate);
+        });
+
+        // 결과 배열 추출
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = stageList.get(i).num;
         }
+
         return answer;
+    }
+
+    // 스테이지 정보 클래스
+    static class Stage {
+        int num;
+        double rate;
+
+        Stage(int num, double rate) {
+            this.num = num;
+            this.rate = rate;
+        }
     }
 }
