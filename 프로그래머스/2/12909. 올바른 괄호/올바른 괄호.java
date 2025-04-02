8bead4c2bc9a9375e3d7a107e5201dd2b0caadcc
김영관 <young.kwin@gmail.com>
2025-04-02 11:13:29 +0900

    [level 2] Title: 올바른 괄호, Time: 20.75 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();     
+        char a[] = s.toCharArray();
+        for(char c : a){
+            if(c == '('){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty())return false;
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
