b2dc138c2af0aaacb9c7f85c3838f378188e1703
김영관 <young.kwin@gmail.com>
2025-05-20 21:27:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 456 ms, Memory: 31088 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,20 @@
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().trim();  // 여기서 바로 trim 적용
-
-        if (s.isEmpty()) {
-            System.out.println(0);
-        } else {
-            String[] s_array = s.split(" ");
-            System.out.println(s_array.length);
-        }
-    }
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		String S = in.nextLine();
+		in.close();
+ 
+		// st 에 공백을 기준으로 나눈 토큰들을 st 에 저장한다
+		StringTokenizer st = new StringTokenizer(S," ");
+		
+		// countTokens() 는 토큰의 개수를 반환한다
+		System.out.println(st.countTokens());	
+		
+	}
+ 
 }
