822ff0d6e8773498df88834097ed16efcdf495c2
김영관 <young.kwin@gmail.com>
2025-05-28 21:27:40 +0900

    [Silver V] Title: 단어 정렬, Time: 464 ms, Memory: 23692 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,27 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashSet;
+import java.io.*;
+import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
+        // 빠른 입출력을 위한 BufferedReader 사용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int N=Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
 
-        HashSet<String> hs = new HashSet<>();
-
-        for(int i=0;i<N;i++)
-            hs.add(br.readLine());
-
-        for(String s : hs){
-
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
         }
-        hs.stream()
-                .sorted(Comparator
-                        .comparingInt(String::length)
-                        .thenComparing(Comparator.naturalOrder()))
-                .forEach(System.out::println);
+
+        List<String> list = new ArrayList<>(set);
+
+        // 정렬 기준: 길이 오름차순 → 길이 같으면 사전 순
+        list.sort((s1, s2) -> {
+            if (s1.length() == s2.length()) {
+                return s1.compareTo(s2);
+            } else {
+                return Integer.compare(s1.length(), s2.length());
+            }
+        });
+
+        // 출력
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
