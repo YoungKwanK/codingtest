8987cd3db8bb2e241fd9a0133925e3221117c532
김영관 <young.kwin@gmail.com>
2025-06-13 10:42:11 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.56 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 5.41 ms
+메모리: 77.9 MB, 시간: 3.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 09:52:34
+2025년 06월 13일 10:42:09
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,8 +1,9 @@
+import java.util.Set;
 import java.util.HashSet;
-
+import java.util.Arrays;
 class Solution{
 	public static int[] solution(int[] numbers){
-		HashSet<Integer> set = new HashSet<>();   //중복값 제거를 위한 해시셋 생성
+		Set<Integer> set = new HashSet<>();   //중복값 제거를 위한 해시셋 생성
 		//두 수를 선택하는 모든 경우의 수를 반복문으로 구함
 		for(int i = 0; i < numbers.length - 1; i++){
 			for(int j = i + 1; j < numbers.length; j++){
@@ -10,7 +11,16 @@ class Solution{
 				set.add(numbers[i] + numbers[j]);
 			}
 		}
-		//해시셋의 값을 오름차순 정렬하고 int[] 형태의 배열로 변환하여 반환
-		return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        // 정답을 저장할 배열 선언
+        int[] answer = new int[set.size()];
+        int index=0;
+		//해시셋의 값을 뽑아 정답 배열에 저장
+        for(int num : set){
+            answer[index]=num;
+            index++;
+        }
+        // 정답 배열을 오름차순 정렬
+        Arrays.sort(answer);
+		return answer;
 	}
 }
