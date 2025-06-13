14d1d417971c00cc3e8e9f2e9a1feb649588dfa9
김영관 <young.kwin@gmail.com>
2025-06-13 11:37:48 +0900

    [Silver IV] Title: 수 찾기, Time: 1116 ms, Memory: 55616 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,40 +1,37 @@
 import java.util.*;
 import java.io.*;
+import java.util.Arrays;
 public class Main {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 정수의 갯수를 저장할 N
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		// N개의 정수를 담을 deque
-		Set<Integer> nums_N = new HashSet<>();
-		
-		// N개의 정수 받기
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			nums_N.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		// 정수의 갯수를 저장할 m
-		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		
-		// M개의 정수 받기
-		int []nums_M = new int[M];
-		
-		// M개의 정수 받기
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			nums_M[i]=Integer.parseInt(st.nextToken());
-		}
-		
-		// 
-		for(int i =0; i<M; i++) {
-			int value = nums_N.contains(nums_M[i])? 1 : 0;
-			System.out.println(value);
-		}
+        
+        // 정수의 갯수 N받기
+        int N = Integer.parseInt(br.readLine());
+        // 정수를 저장할 배열 선언
+        int[] numN = new int[N];
+        // 정수들 받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 받은 정수를 배열에 저장
+        for(int i=0; i<N; i++){
+            numN[i]=Integer.parseInt(st.nextToken());
+        }
+        // 배열 정렬
+        Arrays.sort(numN);
+        
+        // 정수의 갯수 M 받기
+        int M = Integer.parseInt(br.readLine());
+        // 정수들 받기
+        st = new StringTokenizer(br.readLine());
+        // 한번에 받은 정수를 뽑아내며 배열에 있는 지 확인하고 출력
+        for(int i=0; i<M; i++){
+            // 정수 뽑아내기
+            int temp = Integer.parseInt(st.nextToken());
+            // 배열에 정수가 있다면 1출력
+            if(Arrays.binarySearch(numN, temp)>=0) System.out.println(1);
+            // 없다면 0출력
+            else System.out.println(0);
+        }
 	}
+	
 }
