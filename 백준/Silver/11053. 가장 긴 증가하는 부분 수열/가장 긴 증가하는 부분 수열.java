9b5c9a1b618b22e7b501093937e499d2e5063a0a
김영관 <young.kwin@gmail.com>
2025-07-02 14:24:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14476 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N]; // dp[i]: i번째 숫자를 마지막으로 하는 LIS의 길이
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.fill(dp, 1); // 모든 원소의 LIS 최소 길이는 1
+
+        int maxLength = 1;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        System.out.println(maxLength);
+    }
+}
