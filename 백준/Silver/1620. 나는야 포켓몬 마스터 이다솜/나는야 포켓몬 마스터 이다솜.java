8d2e7d5ed983175a1eef91ffd57290ee8a14bd76
Kim Young Kwan <young.kwin@gmail.com>
2026-03-18 20:24:33 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 520 ms, Memory: 56780 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,35 +1,30 @@
 
 import java.util.*;
 import java.io.*;
-public class Main{
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 포켓몬 도감 수 N과 조회할 포켓몬 수 M을 받기
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		// 포켓몬 도감 채울 map선언
-		Map<String, String> poketmon = new TreeMap<>();
-		
-		// 포켓몬 도감에 저장하기 : 양방향 저장
-		for(int i=1; i<=N; i++) {
-			String temp=br.readLine();
-			poketmon.put(String.valueOf(i), temp);
-			poketmon.put(temp, String.valueOf(i));
-		}
-		
-		// 시간 초과를 대비한 StringBuilder 사용
-		StringBuilder sb = new StringBuilder();
-		
-		// 포켓몬 찾기
-		for(int i=0; i<M; i++) {
-			String temp = br.readLine();
-			sb.append(poketmon.get(temp)).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	StringBuilder sb = new StringBuilder();
+    	
+    	// N과 M 받기
+    	st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken());
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	// map 선언하고 포켓몬 도감 만들기
+    	Map<String, String> poketmon = new HashMap<>();
+    	for(int i=1; i<=N; i++) {
+    		String temp = br.readLine(); 
+    		poketmon.put(String.valueOf(i), temp);
+    		poketmon.put(temp, String.valueOf(i));
+    	}
+    	
+    	// 도감에서 찾기
+    	for(int i=0; i<M; i++) {
+    		String find = br.readLine();
+			sb.append(poketmon.get(find)).append("\n");
+    	}
+    	System.out.println(sb);
+    }
 }
