8e0535abc948d3fd374f0430001584d5ddeb045d
Kim Young Kwan <young.kwin@gmail.com>
2026-03-18 19:55:30 +0900

    [Silver V] Title: 집합, Time: 3124 ms, Memory: 313028 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,57 +1,61 @@
 
 import java.util.*;
 import java.io.*;
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	// 공집합 S 설정
+    	Set<Integer> S = new HashSet<>();
+    	
+    	// 연산의 수 M
+    	int M = Integer.parseInt(br.readLine());
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	// 연산의 수만큼 반복
+    	for(int i=0; i<M; i++) {
+    		//연산 받기
+    		st = new StringTokenizer(br.readLine());
+    		
+			// 연산의 명령어 받기
+			String command = st.nextToken();
+			
+			// 명령어 필터링
+			if(command.equals("add")) {
+				S.add(Integer.parseInt(st.nextToken()));
+			}
+			else if(command.equals("remove")) {
+				S.remove(Integer.parseInt(st.nextToken()));
+			}
+			else if(command.equals("check")) {
+				if(S.contains(Integer.parseInt(st.nextToken()))){
+					sb.append('1').append('\n');
+				}
+				else sb.append('0').append('\n');;
+			}
+			else if(command.equals("toggle")) {
+				boolean check=false;
+				int num2 = Integer.parseInt(st.nextToken());
+				for(int num : S) {
+					if(num == num2) {
+						S.remove(num);
+						check = true;
+						break;
+					}
+				}
+				if(!check) S.add(num2);
+			}
+			else if(command.equals("all")) {
+				Set<Integer> temp = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 명령어의 개수 N
-		int N = Integer.parseInt(br.readLine());
-		
-		// 명령어를 저장할 List 선언
-		List<String> set = new LinkedList<>();
-		
-		// 1 ~ 20까지 담을 list
-		List<String> set2 = new LinkedList<>();
-		for(int i=1; i<=20; i++) {
-			set2.add(String.valueOf(i));
-		}
-		
-		// 시간 초과 방지를 위한 StringBuilder
-		StringBuilder sb = new StringBuilder();
-		
-		// 명령어의 개수만큼 반복
-		for(int i=0; i<N; i++) {
-			String[] command = br.readLine().split(" ");
-			
-			// 명령어의 종류에 따른 명령 수행
-			if(command[0].equals("add")) {
-				if(!set.contains(command[1])) set.add(command[1]);
+				S.addAll(temp);
 			}
-			else if(command[0].equals("remove")) {
-				if(set.contains(command[1])) set.remove(command[1]);
-				
+			else {
+				S.clear();
 			}
-			else if(command[0].equals("check")) {
-				if(set.contains(command[1])) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-			}
-			else if(command[0].equals("toggle")) {
-				if(set.contains(command[1])) set.remove(command[1]);
-				else set.add(command[1]);
-			}
-			
-			// 비운 다음에 채워놓기
-			else if(command[0].equals("all")) {
-				set.clear();
-				set.addAll(set2);
-			}
-			else if(command[0].equals("empty")) {
-				set.clear();
-			}
-		}
-		System.out.println(sb);
-	}
-	
+    	}
+    	System.out.println(sb);
+    }
 }
