1effa95a25baa4733c2323e3b7f341b40a35e79a
Kim Young Kwan <young.kwin@gmail.com>
2026-04-08 08:51:44 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 16228 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,66 +1,54 @@
 
 import java.util.*;
 import java.io.*;
-public class Main{
-	
-	static List<Integer>[] map;
-	static boolean[] visited;
-	static boolean check;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		//  정점의 개수 N (1 ≤ N ≤ 100)
-		int N = Integer.parseInt(br.readLine());
-		
-		// 간선 만들기
-		int[][] e = new int[N+1][N+1];
-		for(int i=1; i<N+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<N+1; j++) {
-				e[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		// 간선 연결
-		map = new List[N+1];
-		for(int i=1; i<N+1; i++) {
-			map[i]=new ArrayList<>();
-			for(int j=1; j<N+1; j++) {
-				// 연결이 있으면
-				if(e[i][j]==1) {
-					// 단반향 연결
-					map[i].add(j);
-				}
-			}
-		}
-		
 
-		StringBuilder sb = new StringBuilder();
-		// bfs 모든 노드 간에 시작 bfs(시작노드, 타겟노드)
-		for(int i=1; i<N+1; i++) {
-			for(int j=1; j<N+1; j++) {
-				visited = new boolean[N+1];
-				check = false;
-				dfs(i, j);
-				if(check) {
-					sb.append(1).append(" ");
-				}else sb.append(0).append(" ");
-			}
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+public class Main {
+	static int[][] graph;
+	static boolean[] visited;
+	static int N;
 	
-	static void dfs(int start, int target) {
-		visited[start] = true;
-		for(int i : map[start]) {
-			if(i==target) check=true;
-			if(!visited[i]) dfs(i, target);
-		}
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        // 정점의 수 N
+        N = Integer.parseInt(br.readLine());
+        
+        // 그래프 초기화
+        graph = new int[N][N];
+        for(int i=0; i<N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<N; j++) {
+        		graph[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // 각 정점에 대해 dfs 수행
+        for(int i=0; i<N; i++) {
+        	visited = new boolean[N];
+        	dfs(i);
+        	
+        	// 방문(길이 있었으면) 저장
+        	for(int j=0; j<N; j++) {
+        		if(visited[j]) sb.append("1 ");
+        		else sb.append("0 ");
+        	}
+        	sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    static void dfs(int k) {
+    	for(int i=0; i<N; i++) {
+    		// 방문했거나, 길이 없으면 넘기기
+    		if(visited[i] || graph[k][i] == 0) continue;
+    		else {
+    			visited[i] = true;
+    			dfs(i);
+    		}
+    	}
+    }
 }
