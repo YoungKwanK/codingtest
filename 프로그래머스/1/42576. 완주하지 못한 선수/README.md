d8d9d6785cbb0f4bbd6b8664272cc0005a1b4ab4
Kim Young Kwan <young.kwin@gmail.com>
2026-04-20 08:41:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.33 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 97 MB, 시간: 104.19 ms
+메모리: 96.5 MB, 시간: 88.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 16일 11:53:08
+2026년 04월 20일 08:41:23
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,26 @@
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> people = new HashMap<>();
-        String answer = null;
-
-        // 참가자(동명이인이 있을 경우 value+1)
-        for (String p : participant) {
-            people.put(p, people.getOrDefault(p, 0) + 1);
+        String answer = "";
+        
+        // Hashmap에 넣기
+        Map<String, Integer> people = new HashMap<>();
+        for(String p : participant){
+            people.put(p, people.getOrDefault(p,0)+1);
         }
-
-        // 완주자 제거 (완주하면 그 이름을 가진 사람 수 -1)
-        for (String c : completion) {
-            people.put(c, people.get(c) - 1);
-        }
-
-        // 완주하지 못한 사람 
-        for (String key : people.keySet()) {
-            if (people.get(key) > 0) {
-                answer= key;
+        
+        // HashMap에서 빼기
+        for(String p : completion){
+            int count=people.get(p);
+            if(count==1) people.remove(p);
+            else{
+                people.put(p,count-1);
             }
+        }
+        
+        // 정답 저장
+        for(String p : people.keySet()){
+            answer+=p;
         }
         return answer;
     }
