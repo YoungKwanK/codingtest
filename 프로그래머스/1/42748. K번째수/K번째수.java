87d09b3737ca8b8d51034a42d9a3eb8a80dc55c5
Kim Young Kwan <young.kwin@gmail.com>
2026-04-20 21:05:11 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,22 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        // 명령 개수만큼 반복
-        for(int i=0; i<commands.length; i++){
-            // 자른 배열을 저장할 임시 배열 선언
-            int[] temp = new int[commands[i][1]-commands[i][0]+1];
-            // 자른 배열의 인덱스를 0부터 시작하기 위해
-            int index=0;
-            // 명령 수행
-            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
-                temp[index]=array[j];
-                index++;
+        
+        // commands의 수만큼 반복
+        for(int l=0; l<commands.length; l++){
+            int i = commands[l][0];
+            int j = commands[l][1];
+            int k = commands[l][2];
+            
+            // 자른 값 저장
+            int[] temp = new int[j-i+1];
+            int num=0;
+            for(int o=i-1; o<j; o++){
+                temp[num]=array[o];
+                num++;
             }
-            // 정렬
             Arrays.sort(temp);
-            // k번째 값 저장
-            answer[i]=temp[commands[i][2]-1];
+            answer[l]=temp[k-1];
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 11.48 ms
+메모리: 74.1 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 13일 14:23:16
+2026년 04월 20일 21:05:08
 
 ### 문제 설명
 
