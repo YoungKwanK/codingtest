635279dc9514a67a8118353a034ae53de13c8a67
Kim Young Kwan <young.kwin@gmail.com>
2026-04-20 21:23:26 +0900

    [level 1] Title: 최소직사각형, Time: 2.15 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,23 @@
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        // [0]은 가로, [1]은 세로
-        int[] minSizes = new int[2];
         
-        int width = 0;
-        int height = 0; 
+        // 초깃값 세팅
+        int maxX=0;
+        int maxY=0;
+        int temp;
+        
+        // 명함 사이즈들 정렬
         for(int i=0; i<sizes.length; i++){
-            int temp=0;
-            if(sizes[i][0]<sizes[i][1]){
-                temp=sizes[i][1];
-                sizes[i][1]=sizes[i][0];
-                sizes[i][0]=temp;
+            if(sizes[i][0]>sizes[i][1]){
+                temp = sizes[i][0];
+                sizes[i][0]=sizes[i][1];
+                sizes[i][1]=temp;
             }
-            width = Math.max(width,sizes[i][0]);
-            height = Math.max(height,sizes[i][1]);
+            maxX = Math.max(maxX, sizes[i][0]);
+            maxY = Math.max(maxY, sizes[i][1]);
         }
-        
-        return width*height;
+        answer = maxX * maxY;
+        return answer;
     }
 }
