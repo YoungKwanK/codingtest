2e778f791b2e28c8161b67ebab98a97f08aa86ee
Kim Young Kwan <young.kwin@gmail.com>
2026-04-23 09:14:49 +0900

    [level 3] Title: 가장 먼 노드, Time: 24.09 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,47 +1,46 @@
 import java.util.*;
 class Solution {
-    static List<List<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
-    static int[] count;
-    // bfs를 위한 큐 선언
-    static Queue<Integer> q;
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        // 그래프 초기화
-        for(int i=0; i<n+1; i++){
-            graph.add(new ArrayList<>());
+        // 초깃값 세팅
+        ArrayList<Integer>[] vertex = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            vertex[i] = new ArrayList<>();
         }
-        // 그래프 그리기(양방향 간선 연결)
+        int []visited = new int[n+1];
+        
+        // 간선 양방향 연결
         for(int i=0; i<edge.length; i++){
-            graph.get(edge[i][0]).add(edge[i][1]);
-            graph.get(edge[i][1]).add(edge[i][0]);
+            vertex[edge[i][0]].add(edge[i][1]);
+            vertex[edge[i][1]].add(edge[i][0]);
         }
-        // bfs를 위한 큐 선언
-        q = new LinkedList<>();
-        visited= new boolean[n+1];
-        // 1번 노드에서 가장 멀리 떨어진 노드의 갯수이므로 1부터 시작
-        count = new int[n+1];
-        q.add(1);
-        visited[1]=true;
-        bfs();
-        Arrays.sort(count);
-        int max=count[n];
-        for(int i=1; i<=n; i++){
-            if(count[i]==max) answer++;
+        
+        // bfs
+        Queue<Integer> bfsQ = new LinkedList<>();
+        bfsQ.add(1);
+        visited[1]=1;
+        while(!bfsQ.isEmpty()){
+            int node = bfsQ.poll();
+            
+            for(int i : vertex[node]){
+                if(visited[i]==0){
+                    visited[i] = visited[node]+1;
+                    bfsQ.add(i);
+                }   
+            }
+        }
+        
+        // 최대 거리 찾기 및 개수 세기
+        int max = 0;
+        for (int i : visited) {
+            if (i > max) max = i;
+        }
+
+        for (int i : visited) {
+            if (i == max) answer++;
         }
         
         return answer;
-    }
-    static void bfs(){
-        if(q.isEmpty()) return;
-        int temp = q.poll();
-        for(int num : graph.get(temp)){
-            if(!visited[num]){
-                q.add(num);
-                visited[num]=true;
-                count[num]=count[temp]+1;
-            }
-        }
-        bfs();
     }
 }
