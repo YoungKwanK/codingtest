f7f25c9078b20d8b168b9ee8a2f90b1dd532fdf9
Kim Young Kwan <young.kwin@gmail.com>
2026-04-30 23:54:01 +0900

    [level 2] Title: 전화번호 목록, Time: 327.51 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,12 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
         boolean answer = true;
-        for(int i=0; i<phone_book.length-1; i++){
-            if(phone_book[i].length()<phone_book[i+1].length()&&phone_book[i].equals(phone_book[i+1].substring(0, phone_book[i].length()))){
+        Arrays.sort(phone_book);
+        for(int i=1; i<phone_book.length; i++){
+            if(phone_book[i-1].length() <= phone_book[i].length()&& phone_book[i-1].equals(phone_book[i].substring(0,phone_book[i-1].length()))){
                 answer=false;
+                break;
             }
         }
         return answer;
