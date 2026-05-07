4e6a906caa0e0f1b7c4dcafe31aebdb6ce879651
Kim Young Kwan <young.kwin@gmail.com>
2026-05-07 21:07:55 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        Map<String, Integer> clothesMap = new HashMap<>();
-        // Map에 의상 종류에 따른 개수 넣기
-        for(int i=0; i<clothes.length; i++){
-            clothesMap.put(clothes[i][1], clothesMap.getOrDefault(clothes[i][1], 0)+1);
-        }
-        for(String cloth : clothesMap.keySet()){
-            answer*=(clothesMap.get(cloth)+1);
-        };
         
+        // 자료형 선언 <종류, 갯수>
+        Map<String, Integer> kinds = new HashMap<>();
+        
+        for(String[] cloth : clothes){
+            kinds.put(cloth[1], kinds.getOrDefault(cloth[1], 0)+1);
+        }
+        
+        for(String cloth : kinds.keySet()){
+            answer*=(kinds.get(cloth)+1);
+        }
         return answer-1;
     }
 }
